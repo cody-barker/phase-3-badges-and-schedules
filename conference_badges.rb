1264c1c8322463c04a18f3fbc96c356debfb4817
Cody <codybarker.or@gmail.com>
2023-04-10 14:54:34 -0700

    assign rooms

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(list_of_names)
+    list_of_names.map {|name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms(list_of_speakers)
+    list_of_speakers.map do |name|
+        "Hello, #{name}! You'll be assigned to room #{name.find_index(name)}"
+    end
+end
